6499f9bd79dc30cc53cefc045360e10f020d912e
Knw0n <20255319@esen.edu.sv>
2025-10-30 00:23:12 -0600

    Guia

--- Semana 8/Guia_de_practica.py
+++ Semana 8/Guia_de_practica.py
@@ -63,19 +63,92 @@ print(cont7, cont5)
 
 ##########################################
 
-"""EJERCICIO E"""
+"""EJERCICIO E
 
 N = int(input())
-Pa = int(input())
-Pb = int(input())
-Pc = int(input())
+datos = input().split()
+Pa = int(datos[0])
+Pb = int(datos[1])
+Pc = int(datos[2])
 
 for i in range(N):
     combo = input()
     a = combo.count("A")
     b = combo.count("B")
     c = combo.count("C")
-    combo1 = Pa*a + Pb*b + Pc*c
+    print(Pa*a + Pb*b + Pc*c)
+"""
 
-print(combo1)  
+##########################################
 
+"""EJERCICIO F
+
+N = int(input())
+
+for i in range(N):
+    
+    nombre = input()
+    if len(nombre) <= 6: 
+        print("No vale la pena")
+        
+    elif len(nombre) >= 8:
+        print("Si aguanto otro desarrollo de personaje")
+        
+    else:
+        print("Dios no creo aguantar esta vez")
+"""
+
+##########################################
+
+"""EJERCICIO G
+
+nums = input().split()
+x = int(nums[0])
+y = int(nums[1])
+if x > y:
+    print(x)
+elif x < y:
+    print(y)
+else:
+    print(x)
+"""
+
+##########################################
+
+"""EJERCICIO H
+
+A = int(input())
+entrar = 0
+for i in range(A):
+    edad = int(input())
+    if edad >= 15:
+        entrar += 1
+print(entrar)
+"""
+
+##########################################
+
+""" EJERCICIO I
+
+status = input()
+
+if len(status) <= 9:
+    print("Ola Ivan")
+else: 
+    print("Ol..")
+
+"""
+
+##########################################
+
+"""EJERCICIO J
+
+N = int(input())
+
+for i in range(N):
+    P = int(input())
+    if P >= 3:
+        print("Ok")
+    else: 
+        print("No")
+"""
---
+++ Semana 8/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+N = int(input())
+datos = input().split()
+Pa = int(datos[0])
+Pb = int(datos[1])
+Pc = int(datos[1])
+
+print(datos )
